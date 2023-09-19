217d33536c9e6370d5fc8d080938bf02cda10a8d
ryanctruong <ryanctruong03@gmail.com>
2023-09-19 13:46:07 -0500

    changes

--- app/src/main/java/com/example/lightsout/MainActivity.java
+++ app/src/main/java/com/example/lightsout/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -13,7 +14,53 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int GRID_SIZE = 3;
     private GridLayout grid;
-    private boolean cellState [][];
+    private boolean[][] cellState;
+
+    TextView countLabel;
+
+    Button resetBut, rand;
+
+    View.OnClickListener buttonClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int buttonIndex = grid.indexOfChild(view);
+            int row = buttonIndex / GRID_SIZE;
+            int col = buttonIndex % GRID_SIZE;
+
+            cellState[row][col] = !cellState[row][col];
+
+            if(cellState[row][col]){
+                view.setBackgroundColor(getColor(R.color.blue_500));
+            } else{
+                view.setBackgroundColor(getColor(R.color.black));
+            }
+            updateCounterLabel();
+        }
+    };
+
+    View.OnClickListener resetButton = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            for(int i = 0; i < GRID_SIZE; i++){
+                for(int j = 0; j < GRID_SIZE; j++){
+                    if(cellState[i][j]){
+                        cellState[i][j] = false;
+                        recolor();
+                    }
+                }
+            }
+            updateCounterLabel();
+        }
+    };
+
+    View.OnClickListener random = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            randomize();
+            recolor();
+            updateCounterLabel();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +70,26 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         grid = findViewById(R.id.light_grid);
+        countLabel = findViewById(R.id.scoreCounter);
 
-//      randomize();
 
+
+
+        for(int i = 0; i < grid.getChildCount(); i++){
+            grid.getChildAt(i).setOnClickListener(buttonClick);
+        }
+
+        resetBut = findViewById(R.id.button);
+        resetBut.setOnClickListener(resetButton);
+
+        rand = findViewById(R.id.random);
+        rand.setOnClickListener(random);
+
+
+        randomize();
         recolor();
+        updateCounterLabel();
+
     }
 
     public void recolor(){
@@ -37,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
             int row = i / GRID_SIZE;
             int col = i % GRID_SIZE;
 
-            if (cellState[row][col] == true) {
+            if (cellState[row][col]) {
                 gridButton.setBackgroundColor(getColor(R.color.blue_500));
             } else {
                 gridButton.setBackgroundColor(getColor(R.color.black));
@@ -54,6 +117,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void updateCounterLabel(){
+        String text = "Source: " + count();
+        countLabel.setText(text);
+    }
 
+    public int count(){
+        int count = 0;
+        for(int i = 0; i < GRID_SIZE; i++){
+            for(int j = 0; j < GRID_SIZE; j++){
+                if(cellState[i][j]){
+                    count++;
+                }
+            }
+        }
 
+        return count;
+    }
+
+    public void lightsOff(){
+
+    }
 }
